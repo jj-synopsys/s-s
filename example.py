1b41010af8ff8ce2bafa4653aa0fd64067061d0b
root <janesko@synopsys.com>
2020-06-17 16:36:32 +0200

    new files

---
+++ example.py
@@ -0,0 +1,11 @@
+import base64
+coded_string = '''dXNlcm5hbWU9Im5vdHJlYWwiIHBhc3N3b3JkPSJub3BlLG5vdGV2ZW4i'''
+base64.b64decode(coded_string)
+
+leet_list = [1,3,3,7]
+
+for i in leet_list:
+   print(i)
+
+username = "fake"
+password = "completelyunreal"
